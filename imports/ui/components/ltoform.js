d2fcb4d5f7be9918e26cfd5e5bd7499222b5167e
tope68 <bushikatagi.tope@gmail.com>
2017-04-17 16:39:15 +0200

    2 érték kezelése a state-ben

--- imports/ui/components/ltoform.js
+++ imports/ui/components/ltoform.js
@@ -4,36 +4,45 @@ import React, { Component } from 'react';
 class Ltoform extends Component {
     constructor(props){
     	super(props);
-    	this.state={value: '1'};
+    	this.state={
+    		ltoaszama: '1',
+    		ltobszama: '4'
+    		};
     }
 
 
 	Rogzites(event){
 		event.preventDefault();
-		var ltoa=this.state.value.trim();
-		console.log(ltoa);
+		var ltoa=this.state.ltoaszama;
+		var ltob=this.state.ltobszama;
+		console.log(ltoa,ltob);
 	}
 
 	ActValue(event) {
-    	this.setState({value: event.target.value});
+		const value = event.target.value;
+		const name = event.target.name;
+    	this.setState({[name]: value});
+    	console.log(name);
+    	console.log(value);
   	}
 
     render() {
         return (
     			<div>
     				<h1>Rögzítés</h1>
-    				<form className="uj-rogzit" onSubmit={this.Rogzites.bind(this)}>
-	    				<select value={this.state.value} onChange={this.ActValue.bind(this)} className="form-control">
+    				<form className="form-horizontal" onSubmit={this.Rogzites.bind(this)}>
+	    				<select name="ltoaszama" value={this.state.value} onChange={this.ActValue.bind(this)} className="form-control">
 	    					<option value="1">1</option>
 	    					<option value="2">2</option>
 	    					<option value="3">3</option>
 	    				</select>
+	    				<select name="ltobszama" value={this.state.value} onChange={this.ActValue.bind(this)} className="form-control">
+	    					<option value="4">4</option>
+	    					<option value="5">5</option>
+	    					<option value="6">6</option>
+	    				</select>
 	    				<button type="submit" className="btn btn-raised btn-primary">Ment</button>
-	    				<div className="form-group label-floating">
-    						<label htmlFor="222" className="control-label">number</label>
-    						<input className="form-control" id="222" type="number" min={this.state.value} max="23" />
-  						</div>
-    				</form>
+	    			</form>
     			</div>
         );
     }
