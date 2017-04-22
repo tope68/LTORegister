e4fe08e8cf8b0980e38798b3df98536a59e7369e
Andras Toth <toth.andras@gmail.com>
2017-04-22 20:19:38 +0200

    Fixes #2, nem teszteltem, de elvileg ilyen formatumban kellene

--- imports/api/ltos/methods.js
+++ imports/api/ltos/methods.js
@@ -2,19 +2,21 @@ import {Meteor} from 'meteor/meteor';
 import LTOs from './collection';
 
 Meteor.methods({
-	addRegister({ltoa, ltob}) {
+	addRegister: ({ltoa, ltob}) => {
 		// adat validálás valamivel
 
 
 	  /*  if (! Meteor.userId()) {
       		throw new Meteor.Error('not-authorized'); */
-    }
+    },
 
-    return LTOs.insert({
-    	actualDate: new Date(),
-    	ltoAnumber: ltoa,
-    	ltoBnumber: ltob,
-    	// user: Meteor.userId()
-    });
+    'LTOs.insert': () => {
+        return {
+        	actualDate: new Date(),
+        	ltoAnumber: ltoa,
+        	ltoBnumber: ltob,
+        	// user: Meteor.userId()
+        } 
+    }
  
 });
