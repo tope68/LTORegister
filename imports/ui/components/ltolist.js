75779360752f647b11a97b7d2be6d0622eb80acf
tope68 <bushikatagi.tope@gmail.com>
2017-04-22 22:52:30 +0200

    ez lesz a lista nézet

---
+++ imports/ui/components/ltolist.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Ltolist extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            	<h2>Lista</h2>
+
+            </div>
+   )}
+}
+
+export default Ltolist;
+
