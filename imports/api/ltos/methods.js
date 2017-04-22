1e1d2b8102b95741903c139eb33842222d823cfa
tope68 <bushikatagi.tope@gmail.com>
2017-04-22 21:00:18 +0200

    method szintaktika

--- imports/api/ltos/methods.js
+++ imports/api/ltos/methods.js
@@ -2,20 +2,21 @@ import {Meteor} from 'meteor/meteor';
 import LTOs from './collection.js';
 
 Meteor.methods({
-	addRegister({ltoa, ltob}) {
+	addRegister: ({ltoa, ltob}) => {
 		// adat validálás valamivel
 
 
 	  /*  if (! Meteor.userId()) {
       		throw new Meteor.Error('not-authorized'); */
-    }
 
-    return LTOs.insert({
-    	actualDate: new Date(),
-    	ltoAnumber: ltoa,
-    	ltoBnumber: ltob,
-    	status: 'Aktív',
-    	// user: Meteor.userId()
-    });
+     	return LTOs.insert({
+    		actualDate: new Date(),
+	    	ltoAnumber: ltoa,
+	    	ltoBnumber: ltob,
+	    	status: 'Aktív',
+	    	// user: Meteor.userId() de most csak tesztként valami
+	    	changeBy: 'aktualis user'
+    	});
+    },
  
 });
