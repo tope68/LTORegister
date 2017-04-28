75ccf5a3960ff5a2f3bc5ef62a2e94b6c7429b71
tope68 <bushikatagi.tope@gmail.com>
2017-04-28 18:13:57 +0200

    tablazat de ne nincs benne adat

--- imports/ui/components/ltolist.js
+++ imports/ui/components/ltolist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LTOs from '../../api/ltos/collection.js';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 class Ltolist extends Component {
     constructor(props) {
@@ -10,15 +11,34 @@ class Ltolist extends Component {
     	return LTOs.find().fetch();
     }
 
-    render() {
+        render() {
+        	const dataArray=[this.ltodatas().map((ltodata)=>{
+        		return ltodata
+        	})];
         return (
         	<div>
             	<h2>Lista</h2>
-               	{console.log(LTOs.find({}).fetch())}
+				{console.log(dataArray)}
+
+               	<BootstrapTable data={ dataArray }>
+			        <TableHeaderColumn dataField='id' isKey>ID</TableHeaderColumn>
+			        <TableHeaderColumn dataField='actualDate'>Dátum</TableHeaderColumn>
+			        <TableHeaderColumn dataField='ltoAnumber'>LTO-A</TableHeaderColumn>
+			        <TableHeaderColumn dataField='ltoBnumber'>LTO-B</TableHeaderColumn>
+			        <TableHeaderColumn dataField='status'>Státusz</TableHeaderColumn>
+			        <TableHeaderColumn dataField='changeBy'>User</TableHeaderColumn>
+			        <TableHeaderColumn dataField='changeDate'>Csere Dátuma</TableHeaderColumn>
+				</BootstrapTable>	
+
             	<ul>
             		{this.ltodatas().map((ltodata) => {
             			return <li key={ltodata._id}>
-            						{actualdate=moment(ltodata.actualDate).format('YYYY.MM.DD')} {ltoanumber=ltodata.ltoAnumber} {ltobnumber=ltodata.ltoBnumber} {ltostatus=ltodata.status} {actualuser=ltodata.changeBy} {changedate=moment(ltodata.changeDate).format('YYYY.MM.DD, HH:mm')}
+            						{actualdate=moment(ltodata.actualDate).format('YYYY.MM.DD')}
+            						{ltoanumber=ltodata.ltoAnumber}
+            						{ltobnumber=ltodata.ltoBnumber}
+            						{ltostatus=ltodata.status}
+            						{actualuser=ltodata.changeBy}
+            						{changedate=moment(ltodata.changeDate).format('YYYY.MM.DD, HH:mm')}
             					</li>
                		})}
                	</ul> 
