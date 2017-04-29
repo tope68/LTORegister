4b49074f39b22d452e733ce174d0b650c119b3f3
tope68 <bushikatagi.tope@gmail.com>
2017-04-29 12:37:47 +0200

    tbala sorhoz rendelt nyomogom

--- imports/ui/components/ltolist.js
+++ imports/ui/components/ltolist.js
@@ -11,31 +11,40 @@ class Ltolist extends Component {
     	return LTOs.find().fetch();
     }
 
+	onClickActionButton(cell,row,rowIndex){
+    	console.log(cell,row,rowIndex)
+    }
+
+    actionButton(cell,row,enumObject,rowIndex){
+    	return (<button type="button" onClick={()=>this.onClickActionButton(cell,row,rowIndex)}>Csere{rowIndex}</button>)
+    }
+
         render() {
         	const dataArray=this.ltodatas().map((ltodata)=>{
         		return ltodata
         	});
-
+        	// Dátumformátum magyarul
         	dateFormatDate = (cell,row)=> {
         		return cell=(moment(cell).format('YYYY.MM.DD'))
         	};
-
+        	// Dátum formátum időponttal
 			dateFormatTime = (cell,row)=> {
         		return cell=(moment(cell).format('YYYY.MM.DD, HH:mm'))
         	};
-
+        	// Editálható egy cella clickre
         	const cellEditProp = {
   				mode: 'click',
   				blurToSave: true
 			};        	
-
+			// A lehetséges kazettaszámok még tesztként csak	
 			const ltoNumbers=['1','2','3','4','5','6','7','8','9','10','11'];
-
+			// a kereső mező paraméterei
 			const options = {
   				clearSearch: true,
   				searchPosition: 'right',
   			};
         	
+
         return (
         	<div>
             	<h2>Lista</h2>
@@ -49,7 +58,8 @@ class Ltolist extends Component {
 			        <TableHeaderColumn dataField='status'>Státusz</TableHeaderColumn>
 			        <TableHeaderColumn dataField='changeBy'>User</TableHeaderColumn>
 			        <TableHeaderColumn dataField='changeDate' dataFormat={ dateFormatTime } editable={false}>Csere Dátuma</TableHeaderColumn>
-				</BootstrapTable>	
+			        <TableHeaderColumn dataField='button' dataFormat={this.actionButton.bind(this)} editable={false}></TableHeaderColumn>
+      			</BootstrapTable>	
 
             	<ul>
             		{this.ltodatas().map((ltodata) => {
