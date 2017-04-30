932d0fb8d0138485c00283cbf340708c064f6b98
tope68 <bushikatagi.tope@gmail.com>
2017-04-30 13:39:50 +0200

    route parameter

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,14 +6,15 @@ import {Meteor} from 'meteor/meteor';
 import Mainlayout from '../../ui/layouts/mainlayout.js';
 import Ltoform from '../../ui/components/ltoform.js';
 import Ltolist from '../../ui/components/ltolist.js';
+import Ltolist from '../../ui/components/ltochange.js';
 
 Meteor.startup(()=>{
 	render(
 		<Router history={ browserHistory }>
 			<Route path="/" component={Mainlayout}>
-				
 				<Route path="/ltoform" component={Ltoform}/>
 				<Route path="/ltolist" component={Ltolist}/>
+				<Route path="/ltochange/:id" component={Ltochange}/>
 			</Route>
 		</Router>,
 		document.getElementById('render-target')
