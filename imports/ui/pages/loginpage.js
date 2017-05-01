e8caae75e5413df414a937bf740b831fe409e3c5
tope68 <bushikatagi.tope@gmail.com>
2017-05-01 18:31:43 +0200

    login page

--- imports/ui/pages/loginpage.js
+++ imports/ui/pages/loginpage.js
@@ -1,15 +1,27 @@
 import React, { Component, PropTypes } from 'react';
+import { Accounts, STATES } from 'meteor/std:accounts-ui';
 
 class Login extends Component {
     constructor(props) {
     super(props);
     }
 
+	Accounts.ui.config ({
+	  passwordSignupFields: 'USERNAME_ONLY',
+	  loginPath: '/loginpage',
+	  signUpPath: '/signuppage',
+	  // resetPasswordPath: '/reset-password',
+	  // profilePath: '/profile',
+	  onSignedInHook: () => browserHistory.push('/ltolist'),
+	  onSignedOutHook: () => browserHistory.push('/loginpage'),
+	  minimumPasswordLength: 5
+	});
+
     render() {
         return (
 			<div>
 				<h1>Bejelentkezés</h1>
-				
+				<Accounts.ui.LoginForm />
 			</div>           
         );
     }
