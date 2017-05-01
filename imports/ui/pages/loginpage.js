f01d472bb62cf1cb90158418d23ae3e70ab053f8
tope68 <bushikatagi.tope@gmail.com>
2017-05-01 19:15:43 +0200

    hol a helye

--- imports/ui/pages/loginpage.js
+++ imports/ui/pages/loginpage.js
@@ -3,19 +3,19 @@ import { Accounts, STATES } from 'meteor/std:accounts-ui';
 
 class Login extends Component {
     constructor(props) {
-    super(props);
-    }
+    	super(props);
 
-	Accounts.ui.config ({
-	  passwordSignupFields: 'USERNAME_ONLY',
-	  loginPath: '/loginpage',
-	  signUpPath: '/signuppage',
-	  // resetPasswordPath: '/reset-password',
-	  // profilePath: '/profile',
-	  onSignedInHook: () => browserHistory.push('/ltolist'),
-	  onSignedOutHook: () => browserHistory.push('/loginpage'),
-	  minimumPasswordLength: 5
-	});
+    	Accounts.ui.config ({
+		  passwordSignupFields: 'USERNAME_ONLY',
+		  loginPath: '/loginpage',
+		  signUpPath: '/signuppage',
+		  // resetPasswordPath: '/reset-password',
+		  // profilePath: '/profile',
+		  // onSignedInHook: () => browserHistory.push('/ltolist'),
+		  // onSignedOutHook: () => browserHistory.push('/loginpage'),
+		  minimumPasswordLength: 5
+		)};
+	}
 
     render() {
         return (
